fabe7cd69c6fd7f9b97dea848b7d288e57650721
Caio Matheus <caio.asevedo@gmail.com>
2024-07-08 10:40:55 -0300

    added redis

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ class Application < Sinatra::Base
   configure do
     template = ERB.new File.new('config/database.yml').read
     db_config = YAML.load(template.result(binding), aliases: true)
-    db_config = db_config["#{environment}"]
+    db_config = db_config[environment.to_s]
 
     set :database,
         Sequel.connect(
@@ -17,7 +17,13 @@ class Application < Sinatra::Base
           database: db_config['database'],
           pool: db_config['pool']
         )
-    
+
+    redis_config = YAML.load_file('config/redis.yml')[environment.to_s]
+    set :redis, Redis.new(
+      host: redis_config['host'],
+      port: redis_config['port'],
+    )
+
     enable :logging
   end
 end
